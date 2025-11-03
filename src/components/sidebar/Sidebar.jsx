358e411bcdb4bd2aaca03a1afcd6e1676933b6c3
theHakx <hakapericlive1996@gmail.com>
2025-11-03 19:09:48 +0200

    mobile port

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -6,7 +6,7 @@ import { useState } from 'react'
 
 const variants = {
     open:{
-        clipPath: 'circle(1200px at 50px 50px)',
+        clipPath: 'circle(200vmax at 50px 50px)',
         transition: {
             type:'spring',
             stiffness:20,
